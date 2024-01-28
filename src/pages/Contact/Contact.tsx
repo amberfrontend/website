5a1af404ce6fa7d17cdf63f571f2fddaa2f73f84
Amber Armstrong <54625165+amberfrontend@users.noreply.github.com>
2024-01-27 19:54:25 -0800

    Remove email and Twitter

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -12,10 +12,6 @@ export default function Contact() {
     return (
         <>
             <h1>{pageName}</h1>
-            <h2>Email</h2>
-            <p>
-                &#104;&#101;&#108;&#108;&#111;&#046;&#097;&#109;&#098;&#101;&#114;&#046;&#097;&#114;&#109;&#115;&#116;&#114;&#111;&#110;&#103;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;
-            </p>
             <h2>LinkedIn</h2>
             <p>
                 <Link
@@ -24,16 +20,6 @@ export default function Contact() {
                     type="BODY"
                 >
                     mizamberarmstrong
-                </Link>
-            </p>
-            <h2>Twitter</h2>
-            <p>
-                <Link
-                    href="https://twitter.com/amber_a11y"
-                    external
-                    type="BODY"
-                >
-                    @amber_a11y
                 </Link>
             </p>
         </>
