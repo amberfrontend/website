ed82e152020b84a87ae9e7a2df89d49cb3d9647d
Amber Armstrong <hello.amber.armstrong@gmail.com>
2024-03-17 10:29:38 -0400

    Throw selectors into a constant and add roles

---
+++ src/constants/constants.ts
@@ -0,0 +1,2 @@
+export const FOCUSABLE_SELECTORS =
+  'a,button,input,textarea,[role="button"],[role="link"],[role="input"],[role="textbox"],[role="search"],[role="checkbox"],[role="radio"],[role="combobox"],[role="option"],[tabindex="0"]';
